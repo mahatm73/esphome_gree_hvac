fae240712cc0e804a632472f09c50bf19d1c4def
mahatm73 <Evgeniy@Annenkov.org>
2025-07-23 14:48:41 +0300

    4

--- components/gree/climate.py
+++ components/gree/climate.py
@@ -1,8 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate, uart
-from esphome.components.climate.climate import SUPPORT_SWING_MODE, CLIMATE_SWING_OFF, CLIMATE_SWING_ON
-from esphome.const import CONF_ID  
+from esphome.const import CONF_ID
 
 DEPENDENCIES = ['uart']
 
@@ -22,15 +21,14 @@ PLATFORM_SCHEMA = climate.climate_schema({
 })
 
 async def to_code(config):
-    # Instantiate the component
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await climate.register_climate(var, config)
 
-    # Set UART bus
+    # UART bus
     cg.add(var.set_uart_id(config[CONF_UART_ID]))
 
-    # Configure optional swing commands
+    # Swing commands
     if CONF_SWING_ON_COMMAND in config:
         cg.add(var.set_swing_on_command(config[CONF_SWING_ON_COMMAND]))
     if CONF_SWING_OFF_COMMAND in config:
@@ -40,46 +38,44 @@ async def to_code(config):
 
 
 def to_hass_config(data, config):
-    # Base climate config
     hass = climate.core_to_hass_config(data, config)
-    # Enable swing support
-    hass['supported_features'] |= SUPPORT_SWING_MODE
-    hass['swing_modes'] = [CLIMATE_SWING_OFF, CLIMATE_SWING_ON]
+    hass['supported_features'] |= climate.SUPPORT_SWING_MODE
+    hass['swing_modes'] = [climate.CLIMATE_SWING_OFF, climate.CLIMATE_SWING_ON]
     return hass
 
-# Python wrapper class
+# Python wrapper
 class GreeClimate(climate.Climate, cg.Component, cg.PollingComponent):
     """Gree HVAC Climate integration with swing support"""
     def __init__(self) -> None:
         super().__init__()
         self._uart = None
-        self._swing_on_command = None
-        self._swing_off_command = None
+        self._swing_on = None
+        self._swing_off = None
 
     def set_uart_id(self, uart_var) -> None:
         self._uart = uart_var
 
     def set_swing_on_command(self, code: int) -> None:
-        self._swing_on_command = code
+        self._swing_on = code
 
     def set_swing_off_command(self, code: int) -> None:
-        self._swing_off_command = code
+        self._swing_off = code
 
     def control(self, call):
-        # Send mode, temperature, fan commands
+        # Standard commands
         self._send_mode_and_temp(call)
         self._send_fan_mode(call)
 
-        # Send swing command if configured
-        swing = call.get_swing_mode()
-        if swing is not None and self._uart is not None:
-            if swing == CLIMATE_SWING_OFF and self._swing_off_command is not None:
-                self._uart.write_byte(self._swing_off_command)
-            elif swing == CLIMATE_SWING_ON and self._swing_on_command is not None:
-                self._uart.write_byte(self._swing_on_command)
+        # Swing (louver)
+        swing_mode = call.get_swing_mode()
+        if swing_mode is not None and self._uart is not None:
+            if swing_mode == climate.CLIMATE_SWING_ON and self._swing_on is not None:
+                self._uart.write_byte(self._swing_on)
+            elif swing_mode == climate.CLIMATE_SWING_OFF and self._swing_off is not None:
+                self._uart.write_byte(self._swing_off)
 
     def dump_config(self) -> None:
         super().dump_config()
         _LOGGER = cg.get_logger(__name__)
-        _LOGGER.info("  Swing ON command: %s", self._swing_on_command)
-        _LOGGER.info("  Swing OFF command: %s", self._swing_off_command)
+        _LOGGER.info("  Swing ON command: %s", self._swing_on)
+        _LOGGER.info("  Swing OFF command: %s", self._swing_off)
